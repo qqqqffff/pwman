681a5442c53f731d6ab147604f22216e6cd387bd
apollo <1apollo.rowe@gmail.com>
2023-05-07 23:48:19 -0400

    init

---
+++ src/login/Login.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Login.css';
+import {login} from '../mongoserver.js'
+
+function Login() {
+  return (
+    <div className="Login">
+      <div >
+          <LoginButton/>
+      </div>
+    </div>
+  );
+}
+
+function LoginButton(){
+    function attemptLogin(){
+        const loginResponse: number = login("someUser", "somePassword");
+        if(loginResponse === 1){
+            //do something
+        }
+        else if(loginResponse === 2){
+            //do something else
+        }
+    }
+    return(
+        <button onClick={attemptLogin}>
+
+        </button>
+    )
+}
+
+export default Login;

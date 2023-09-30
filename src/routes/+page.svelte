eb5c79eebafd4da26697740055cfaba7ae5e6544
apollo <1apollo.rowe@gmail.com>
2023-09-30 13:46:17 -0500

    Added NavBar to home page with some styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
-<nav>
-    <a href='/' class='text-3xl'>Home</a>
-    
+<nav class='bg-zinc-700 p-4'>
+    <div class='flex flex-nowrap justify-between'>
+        <a href='/' class='text-xl text-blue-100 hover:underline self-center ml-5'>PWMan</a>
+        <div class='relative'>
+            <div class='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
+                <svg class="w-4 h-4 text-zinc-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                </svg>
+            </div>
+            <input type='search' id='search' class='block w-96 pl-10 p-4 text-sm text-blue-100 border border-zinc-800 rounded-lg bg-zinc-600 focus:ring-blue-500 focus:border-blue-500' placeholder="Search Passwords"/>
+        </div>
+        <a href='/profile' class='text-xl text-blue-100 hover:underline self-center mr-5'>Profile</a>
+    </div>
 </nav>

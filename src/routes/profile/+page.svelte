31ae86bf14426bd108f98816aa986743a7fcad53
Apollo Rowe <1apollo.rowe@gmail.com>
2023-10-03 20:30:15 -0400

    Added logout function and removed auth route directory

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { goto, invalidateAll } from "$app/navigation";
+    import { getAuth, signOut } from "firebase/auth";
+
+    const logout = async () => {
+        const firebaseAuth = getAuth();
+        await signOut(firebaseAuth);
+        await fetch('/logout', { method: 'POST' });
+        await invalidateAll();
+        goto('/login');
+    }
+</script>
